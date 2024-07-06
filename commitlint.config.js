c5fdd947b024a8065f62b2756429fe5e23835077
yujin123 <1425816423@qq.com>
2024-07-06 15:13:47 +0800

    chore(commitlint): update type-enum list and add test script
    - Extend type-enum array with new commit types.
    - Refactor commitlint config to use local typeEnum constant.
    - Add test script to package.json for future testing purposes.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,25 @@
 /**
  * https://commitlint.js.org/reference/rules.html
  */
+
+const typeEnum = [
+	'feature',
+	'fix',
+	'cli',
+	'share',
+	'docs',
+	'renderer',
+	'perf',
+	'refactor',
+	'perf',
+	'revert',
+	'test',
+	'chore',
+];
+
 export default {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
-		'type-enum': [
-			2,
-			'always',
-			[
-				'feature',
-				'fix',
-				'docs', 
-                'style', 
-                'refactor', 
-                'perf', 
-                'test', 
-                'chore'
-			],
-		],
+		'type-enum': [2, 'always', typeEnum],
 	},
 };
