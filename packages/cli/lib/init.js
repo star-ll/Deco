1b9ac1e0db058dcaf9863b09ff16e4365a0224c5
yujin123 <1425816423@qq.com>
2024-08-31 14:04:38 +0800

    refactor(cli): 重构cli

--- packages/cli/lib/init.js
+++ packages/cli/lib/init.js
@@ -4,8 +4,20 @@ import inquirer from 'inquirer';
 import figlet from 'figlet';
 import fs from 'fs-extra';
 import { exec } from 'node:child_process';
+import ora from 'ora';
 
 const { mkdirp, outputJson, emptyDir, pathExists, copy, readJson } = fs;
+
+class ArgsContext {
+	constructor(projectName) {
+		this.cwd = process.cwd();
+		this.__filename = url.fileURLToPath(import.meta.url);
+		this.__dirname = path.dirname(__filename);
+		this.projectName = projectName;
+		this.projectPath = path.join(this.cwd, projectName);
+		this.packageManagerTool = 'npm';
+	}
+}
 
 function runStartUI() {
 	return new Promise((resolve, reject) => {
@@ -20,7 +32,7 @@ function runStartUI() {
 		});
 	});
 }
-function runType() {
+function ensureProjectName() {
 	return inquirer
 		.prompt([
 			{
@@ -42,15 +54,8 @@ function runType() {
 		});
 }
 
-async function main() {
-	await runStartUI();
-	const command = await runType();
-
-	const cwd = process.cwd();
-	const __filename = url.fileURLToPath(import.meta.url);
-	const __dirname = path.dirname(__filename);
-	const { projectName } = command;
-	const projectPath = path.join(cwd, projectName);
+async function ensureProjectPath({ projectPath, projectName }) {
+	const execContext = { desc: 'Make project directory', callback: () => {} };
 
 	if (await pathExists(projectPath)) {
 		const { action } = await inquirer.prompt({
@@ -66,7 +71,8 @@ async function main() {
 
 		switch (action) {
 			case 'overwrite':
-				await emptyDir(projectPath);
+				execContext.callback = async () => emptyDir(projectPath);
+				execContext.desc = 'Removing existing files';
 				break;
 			case 'skip':
 				break;
@@ -74,27 +80,73 @@ async function main() {
 				return process.exit();
 		}
 	} else {
-		mkdirp(path.join(cwd, projectName)).catch((err) => {
-			console.error(err);
-		});
+		execContext.desc = 'Creating project directory';
+		execContext.callback = async () =>
+			mkdirp(projectPath).catch((err) => {
+				console.error(err);
+			});
 	}
 
-	// create ../template/package.json
-	const targetPackage = await readJson(path.join(__dirname, '../template/package.json'));
-	targetPackage.name = projectName;
-	await outputJson(path.join(projectPath, 'package.json'), targetPackage).catch((err) => {
-		console.error('create package.json file fail');
-		console.error(err);
-	});
+	return execContext;
+}
 
+async function execCopyConfigFile({ projectPath }) {
 	// copy ../template/webpack.config.js
-	await copy(path.join(__dirname, '../template/webpack.config.js'), path.join(projectPath, 'webpack.config.js')).catch(
-		(err) => {
-			console.error('copy package.json file fail');
-			console.error(err);
-		},
-	);
+	const execContext = {
+		desc: 'Create webpack.config.js file',
+		callback: (execContext.callback = async () =>
+			copy(
+				path.join(__dirname, '../template/webpack.config.js'),
+				path.join(projectPath, 'webpack.config.js'),
+			).catch((err) => {
+				console.error('copy package.json file fail');
+				console.error(err);
+			})),
+	};
 
+	return execContext;
+}
+
+async function execCreatePackageJson({ projectName, projectPath }) {
+	// create ../template/package.json
+	const execContext = {
+		desc: 'Create package.json file',
+		callback: async () => {
+			// const targetPackage = await readJson(path.join(__dirname, '../template/package.json'));
+			// targetPackage.name = projectName;
+			// return outputJson(path.join(projectPath, 'package.json'), targetPackage).catch((err) => {
+			// 	console.error('create package.json file fail');
+			// 	console.error(err);
+			// });
+		},
+	};
+	return execContext;
+}
+
+async function execInstall({ packageManagerTool, projectPath }) {
+	return {
+		desc: 'Install dependencies',
+		callback: async () =>
+			exec(
+				`${packageManagerTool} install`,
+				{
+					cwd: projectPath,
+					shell: true,
+					// stdio: 'inherit',
+				},
+				(error, stdout, stderr) => {
+					if (error) {
+						console.error(`exec error: ${error}`);
+						return;
+					}
+					console.log(`stdout: ${stdout}`);
+					console.error(`stderr: ${stderr}`);
+				},
+			),
+	};
+}
+
+async function ensurePackageManagerTool(argsContext) {
 	const { action } = await inquirer.prompt({
 		type: 'list',
 		name: 'action',
@@ -106,23 +158,45 @@ async function main() {
 		],
 	});
 
-	console.log(projectPath, action);
-	exec(
-		`${action} install`,
-		{
-			cwd: projectPath,
-			shell: true,
-			// stdio: 'inherit',
-		},
-		(error, stdout, stderr) => {
-			if (error) {
-				console.error(`exec error: ${error}`);
-				return;
+	argsContext.packageManagerTool = action;
+}
+
+async function execAll(execQueue, argsContext) {
+	const spinner = ora('Loading unicorns');
+	try {
+		for (const execTask of execQueue) {
+			if (!execTask) {
+				continue;
 			}
-			console.log(`stdout: ${stdout}`);
-			console.error(`stderr: ${stderr}`);
-		},
-	);
+			spinner.text(execTask.desc + '...').start();
+			await execTask(argsContext);
+			spinner.succeed(execTask.desc + ' OK');
+		}
+	} catch (err) {
+		spinner.fail('Fail!! Please check fail log.');
+		console.error(err);
+	}
+}
+
+async function main() {
+	await runStartUI();
+	const command = await ensureProjectName();
+
+	const { projectName } = command;
+	const argsContext = new ArgsContext(projectName);
+
+	const ensureQueue = [
+		ensureProjectPath,
+		ensurePackageManagerTool,
+		execCreatePackageJson,
+		execCopyConfigFile,
+		execInstall,
+	];
+	const execQueue = [];
+	for (const task of ensureQueue) {
+		execQueue.push(await task(argsContext));
+	}
+	await execAll(execQueue, argsContext);
 }
 
 export default main;
