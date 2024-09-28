949b7a285bd23ed490b8e45b90e7dc895003ae00
yujin123 <1425816423@qq.com>
2024-09-28 15:13:48 +0800

    fix(cli): npm create decoco

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,24 +1,5 @@
 #!/usr/bin/env node
 
 import init from "./lib/init.js"
-import fs from 'fs-extra';
-import path from 'path';
-import url from 'url';
-import { Command } from 'commander';
 
-// const cwd = process.cwd();
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const { version, description, name } = await fs.readJson(path.join(__dirname, './package.json'));
-const program = new Command();
-
-program.name(name).description(description).version(version);
-
-program
-	.command('init')
-	.description('Initialize a decoco project')
-	.action((str, options) => {
-		init()
-	});
-
-program.parse();
+init()
