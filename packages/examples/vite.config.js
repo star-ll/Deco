ea5148f29cdf6a7373d7b97ec3daf6f4644be9bb
starry-sky <65382511+star-ll@users.noreply.github.com>
2025-02-16 13:32:33 +0800

    Revert "refactor(core): Delete StatePool.initState"

--- packages/examples/vite.config.js
+++ packages/examples/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import decoInjectComponent from '@decoco/rollup-plugin-auto-inject-component';
-import dts from "@decoco/rollup-plugin-dts"
 
 const config = defineConfig({
 	base: './',
-	plugins: [dts(),decoInjectComponent(['src/**/*.(tsx|jsx)'])],
+	plugins: [decoInjectComponent()],
 	build: {
 		lib: {
 			entry: './index.ts',
