05099aef8c872759dae196169da09d54755bdbd2
yujin123 <1425816423@qq.com>
2025-02-16 13:14:56 +0800

    feature(plugin): add dts plugin

--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import decoInjectComponent from '@decoco/rollup-plugin-auto-inject-component';
+import dts from "@decoco/rollup-plugin-dts"
 
 const config = defineConfig({
 	base: './',
-	plugins: [decoInjectComponent()],
+	plugins: [dts(),decoInjectComponent(['src/**/*.(tsx|jsx)'])],
 	build: {
 		lib: {
 			entry: './index.ts',
