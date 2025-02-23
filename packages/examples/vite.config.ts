27433572427beaf4a46110e2fba3f8499ae3a813
yujin123 <1425816423@qq.com>
2025-02-23 13:06:35 +0800

    feature(plugin): @decoco/rollup-plugin-macros

--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import decoInjectComponent from '@decoco/rollup-plugin-auto-inject-component';
-import macors from "@decoco/rollup-plugin-macors"
+import macros from "@decoco/rollup-plugin-macros"
 
 const config = defineConfig({
 	base: './',
-	plugins: [macors(),decoInjectComponent(['src/**/*.(tsx|jsx)'])],
+	plugins: [macros(),decoInjectComponent(['src/**/*.(tsx|jsx)'])],
 	build: {
 		lib: {
 			entry: './index.ts',
